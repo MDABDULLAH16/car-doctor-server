d29c9d5464b4395d5009807a95df6a03a4a8d71d
MD ABDULLAH <mdabdullah161036@gmail.com>
2024-02-26 19:14:48 +0600

    bookings data load by using user email

--- index.js
+++ index.js
@@ -51,6 +51,15 @@ async function run() {
       res.send(result);
     });
 
+    app.get("/bookings", async (req, res) => {
+      let query = {};
+      if (req.query?.email) {
+        query = { email: req.query.email };
+      }
+      const result = await bookingsCollection.find(query).toArray();
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
