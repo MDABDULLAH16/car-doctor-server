bc98861648d1f4636d7aa665eace4a160a604e0d
MD ABDULLAH <mdabdullah161036@gmail.com>
2024-03-04 11:31:51 +0600

    jwt token supply

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const express = require("express");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
 //middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
 app.use(express.json());
+app.use(cookieParser());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7jr1vgq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -27,6 +35,24 @@ async function run() {
 
     const serviceCollection = client.db("carDoctorDB").collection("services");
     const bookingsCollection = client.db("carDoctorDB").collection("bookings");
+
+    //auth api
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      console.log(user);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN, {
+        expiresIn: "1h",
+      });
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: false,
+          // sameSite: "none",
+        })
+        .send({ success: true });
+    });
+
+    //service api
     app.get("/services", async (req, res) => {
       const result = await serviceCollection.find().toArray();
       res.send(result);
@@ -52,6 +78,7 @@ async function run() {
     });
 
     app.get("/bookings", async (req, res) => {
+      console.log("tok tok token", req.cookies.token);
       let query = {};
       if (req.query?.email) {
         query = { email: req.query.email };
